3530d35d1ceb256450067e2b083319bcbf8b4023
12008yz <101268985+12008yz@users.noreply.github.com>
2024-11-26 12:10:58 +0300

    Всё огонь

--- models/Case.js
+++ models/Case.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const Item = require('./Item');
 
 const Case = sequelize.define('Case', {
    title: {
